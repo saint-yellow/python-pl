1608e29d38cd4f4f470b3d6abc6736c7e4e1b9ea
saint-yellow <saint-yellow@hotmail.com>
2021-06-15 18:36:42 +0800

    commit solutions of problem #24

---
+++ leetcode/problem-24.py
@@ -0,0 +1,34 @@
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        return self.method1(head)
+
+    # 基于双指针的迭代法
+    def method1(self, head: ListNode) -> ListNode:
+        slow: ListNode = None
+        fast: ListNode = head
+        while fast:
+            temp: ListNode = fast.next
+            fast.next = slow
+            slow = fast
+            fast = temp
+        return slow
+
+    # 递归法
+    def method2(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+
+        ret: ListNode = self.method2(head.next)
+        head.next.next = head
+        head.next = None
+        return ret
+
+    def method3(self, head: ListNode) -> ListNode:
+        pass
