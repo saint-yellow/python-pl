ef46e518501e75c0d3d48736059a042e145edbe2
saint-yellow <saint-yellow@hotmail.com>
2021-06-30 03:06:54 +0800

    commit solutions with comments for problems

---
+++ leetcode/problem-19.py
@@ -0,0 +1,61 @@
+# LeetCode Problem #19: 删除链表的倒数第N个结点
+
+# Definition for singly-linked list.
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        return self.method1(head, n)
+
+
+    # 遍历两次
+    def method1(self, head: ListNode, n: int) -> ListNode:
+        L = self.__getLength(head)
+        sentinel = ListNode(-1, head)
+        current = sentinel
+        for _ in range(1, L+1-n):
+            current = current.next
+        current.next = current.next.next
+        return sentinel.next
+
+
+    def __getLength(self, head: ListNode) -> int:
+        count = 0
+        while head is not None:
+            count += 1
+            head = head.next
+        return count
+
+    # 栈
+    def method2(self, head: ListNode, n: int) -> ListNode:
+        sentinel = ListNode(-1, head)
+        current = sentinel
+        stack = []
+        while current:
+            stack.append(current)
+            current = current.next
+        for _ in range(n):
+            stack.pop()
+        node = stack.pop()
+        node.next = node.next.next
+        return sentinel.next
+
+    # 双指针
+    def method3(self, head: ListNode, n: int) -> ListNode:
+        sentinel = ListNode(-1, head)
+        slow = sentinel
+        fast = sentinel.next
+        for _ in range(n):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return sentinel.next
