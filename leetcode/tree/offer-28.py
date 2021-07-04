4797a81e515efe8b02e95d33479ef1d7291941ce
黄炯辉 <saint-yellow@hotmail.com>
2021-07-04 19:29:52 +0800

    commit solution(s) (with comments) for problems

---
+++ leetcode/tree/offer-28.py
@@ -0,0 +1,44 @@
+# 剑指Offer Problem Nr. 28: 对称的二叉树
+
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.method2(root)
+
+    # 递归
+    def method1(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.method1(p.left, q.right) and self.method1(p.right, q.left)
+
+
+    # 迭代
+    def method2(self, root: TreeNode) -> bool:
+        node1, node2 = root, root
+        queue = [node1, node2]
+        while len(queue) > 0:
+            node1 = queue.pop(0)
+            node2 = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            queue.append(node1.left)
+            queue.append(node2.right)
+            queue.append(node1.right)
+            queue.append(node2.left)
+        return True
---
+++ leetcode/tree/problem-112.py
@@ -0,0 +1,48 @@
+# LeetCode Problem Nr. 112: 路径总和
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        return self.method1(root, targetSum)
+
+    def method1(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.left and not root.right:
+            return targetSum == root.val
+
+        left = self.method1(root.left, targetSum-root.val) 
+        right = self.method1(root.right, targetSum-root.val)
+        return left or right
+
+    def method2(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+
+        nodeQueue = [root]
+        valueQueue = [root.val]
+
+        while nodeQueue:
+            length = len(nodeQueue)
+            for _ in range(length):
+                node = nodeQueue.pop(0)
+                value = valueQueue.pop(0)
+                if not node.left and not node.right:
+                    if value == targetSum:
+                        return True
+                if node.left:
+                    nodeQueue.append(node.left)
+                    valueQueue.append(value+node.left.val)
+                if node.right:
+                    nodeQueue.append(node.right)
+                    valueQueue.append(value+node.right.val)
+        return False
+            
+        
