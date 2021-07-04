e55add195258733cfc056e59481e079919791a0b
黄炯辉 <saint-yellow@hotmail.com>
2021-07-04 16:21:57 +0800

    delete wrong solution(s)

--- leetcode/tree/problem-111.py
+++ leetcode/tree/problem-111.py
@@ -15,34 +15,9 @@ class Solution:
 
     # 自顶向下的解法
     def method1(self, node: TreeNode, depth: int) -> int:
-        '''
-        node: 当前遍历到的节点
-        depth: 遍历到当前节点前所算得的最大深度
-        '''
-
-        # 空节点
-        if node is None:
-            return depth
-
-        # 叶子节点
-        if node.left is None and node.right is None:
-            return depth+1
-
-        # 计算左、右两子树的最大深度，并取其最大者
-        leftDepth = self.method1(node.left, depth+1)
-        rightDepth = self.method1(node.right, depth+1)
-        return min(leftDepth, rightDepth)
+        pass
 
     # 自底向上的解法
     def method2(self, node: TreeNode) -> int:
-        # 空二叉树
-        if node is None:
-            return 0
-
-        leftDepth = self.method2(node.left)
-        rightDepth = self.method2(node.right)
-
-        # 一棵二叉树的最大深度 = 子树的最小深度 + 1 
-        # 1代表根节点的深度
-        return min(leftDepth, rightDepth) + 1
+       pass
 
