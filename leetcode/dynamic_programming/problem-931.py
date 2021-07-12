a0c4c7da71cc59315f5273da2db5c7ba179af166
黄炯辉 <saint-yellow@hotmail.com>
2021-07-12 23:28:59 +0800

    commit solution(s) (with comments) for problems

--- leetcode/dynamic_programming/problem-931.py
+++ leetcode/dynamic_programming/problem-931.py
@@ -15,11 +15,11 @@ class Solution:
         for i in range(1, n):
             for j in range(n):
                 if j == 0:
-                    matrix[i][j] = min(matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j]
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1])
                 elif j == n-1:
-                    matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j]) + matrix[i][j]
+                    matrix[i][j] += min(matrix[i-1][j-1], matrix[i-1][j])
                 else:
-                    matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j]
+                    matrix[i][j] += min(matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1])
         return min(matrix[n-1])
 
 
