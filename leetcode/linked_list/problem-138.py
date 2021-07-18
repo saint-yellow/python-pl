2b747fb88ab96d55c48b1387a482e53d75eb7c48
黄炯辉 <saint-yellow@hotmail.com>
2021-07-19 07:38:44 +0800

    commit solution(s) (with comments) for problems

---
+++ leetcode/linked_list/problem-138.py
@@ -0,0 +1,34 @@
+# LeetCode Problem Nr. 138: 复制带随机指针的链表
+
+from typing import Dict, List, Union
+
+from ds import SinglyLinkedNode
+
+
+class Node(SinglyLinkedNode):
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val: int = x
+        self.next: Node = next
+        self.random: Node = random
+
+
+class Solution:
+    def copyRandomList(self, head: Node) -> Node:
+        return self.__method1(head)
+
+    def __method1(self, head: Node) -> Node:
+        hasMap: Dict[Node, Node] = {}
+        pointer = head
+
+        while pointer:
+            hasMap[pointer] = Node(pointer.val)
+            pointer = pointer.next
+
+        pointer = head
+        while pointer:
+            node = hasMap.get(pointer, None)
+            node.next = hasMap.get(pointer.next, None)
+            node.random = hasMap.get(pointer.random, None)
+            pointer = pointer.next
+
+        return hasMap(head, None)
