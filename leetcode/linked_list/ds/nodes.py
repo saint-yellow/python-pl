0bf2a02a734d1d5eb8f9429d2422ca738b4edf40
黄炯辉 <saint-yellow@hotmail.com>
2021-07-18 18:51:53 +0800

    commit solution(s) (with comments) for problems

--- leetcode/linked_list/ds/nodes.py
+++ leetcode/linked_list/ds/nodes.py
@@ -3,9 +3,17 @@ from typing import List
 
 # Definition for singly-linked list.
 class SinglyLinkedNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self, val: int = 0, next: 'SinglyLinkedNode' = None):
+        self.val: int = val
+        self.next: SinglyLinkedNode = next
+
+    @staticmethod
+    def listAllValues(head: 'SinglyLinkedNode') -> List[int]:
+        result = []
+        while head:
+            result.append(head.val)
+            head = head.next
+        return result
 
 
 class DoublyLinkedNode:
@@ -14,6 +22,13 @@ class DoublyLinkedNode:
         self.prev = prev
         self.next = next
 
+    @staticmethod
+    def listAllValues(head: 'DoublyLinkedNode') -> List[int]:
+        result = []
+        while head:
+            result.append(head.val)
+            head = head.next
+        return result
 
 def buildSinglyLinkedList(values: List[int]):
     if not values:
---
+++ leetcode/linked_list/problem-1838.py
@@ -0,0 +1,38 @@
+# LeetCode Problem Nr. 1838: 旋转链表
+
+from typing_extensions import TypeAlias
+
+from ds import SinglyLinkedNode, buildSinglyLinkedList, listAllValues
+
+ListNode: TypeAlias = SinglyLinkedNode
+
+
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        return self.__method1(head, k)
+
+    def __method1(self, head: ListNode, k: int) -> ListNode:
+        if not (head and head.next):
+            return head
+
+        n = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            n += 1
+        tail.next = head
+
+        step = n - k % n - 1
+        pointer = head
+        for _ in range(step):
+            pointer = pointer.next
+        newHead = pointer.next
+        pointer.next = None
+        return newHead
+
+
+if __name__ == '__main__':
+    s = Solution()
+    for k in range(1, 6):
+        linkedList = buildSinglyLinkedList([1, 2, 3, 4, 5, 6, 7, 8])
+        print(listAllValues(s.rotateRight(linkedList, k)))
