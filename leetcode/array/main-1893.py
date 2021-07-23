432cccc7f96a79c7a47bdb4a2a58fc534c5f55c3
黄炯辉 <saint-yellow@hotmail.com>
2021-07-23 18:25:33 +0800

    commit solution(s) (with comments) for problems

---
+++ leetcode/array/main-1893.py
@@ -0,0 +1,29 @@
+# LeetCode Problem Nr. 1893: 检查是否区域内所有整数都覆盖
+
+from typing import List
+
+
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        return self.__method1(ranges, left, right)
+
+    def __method1(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        n = len(ranges)
+        while left <= right:
+            for i in range(n):
+                if left in range(ranges[i][0], ranges[i][1] + 1):
+                    break
+
+                if i == n - 1 and left not in range(ranges[i][0], ranges[i][1] + 1):
+                    return False
+
+            left += 1
+        return True
+
+
+if __name__ == '__main__':
+    ranges = [[1, 10], [10, 20]]
+    left, right = 21, 21
+
+    s = Solution()
+    print(s.isCovered(ranges, left, right))
---
+++ leetcode/tree/main-235.py
@@ -0,0 +1,27 @@
+# LeetCode Problem Nr. 235: 二叉搜索树的最近公共祖先
+
+from typing_extensions import TypeAlias
+
+from ds import BinaryNode
+
+TreeNode: TypeAlias = BinaryNode
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.__method1(root, p, q)
+
+    def __method1(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or root == p or root == q:
+            return root
+
+        left = self.__method1(root.left, p, q)
+        right = self.__method1(root.right, p, q)
+
+        if left and right:
+            return root
+        if left and not right:
+            return left
+        if not left and right:
+            return right
+        return None
