55532105f0433063bc035a90166d14820b5c02ea
黄炯辉 <saint-yellow@hotmail.com>
2021-07-23 18:50:50 +0800

    commit solution(s) (with comments) for problems

---
+++ leetcode/tree/main-701.py
@@ -0,0 +1,23 @@
+# LeetCode Problem Nr. 701: 二叉搜索树中的插入操作
+
+from typing_extensions import TypeAlias
+
+from ds import BinaryNode
+
+TreeNode: TypeAlias = BinaryNode
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        return self.__method1(root, val)
+
+    def __method1(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self.__method1(root.left, val)
+        if val > root.val:
+            root.right = self.__method1(root.right, val)
+
+        return root
