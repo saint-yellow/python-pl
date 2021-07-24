f2715179e4ccae1ac90f75d36580b4f0f136e40d
黄炯辉 <saint-yellow@hotmail.com>
2021-07-24 19:06:45 +0800

    commit solution(s) (with comments) for problems

---
+++ leetcode/tree/main-958.py
@@ -0,0 +1,32 @@
+# LeetCode Problem Nr. 958: 二叉树的完全性检验
+
+from typing import List, Tuple
+
+from typing_extensions import TypeAlias
+
+from ds import BinaryNode
+
+TreeNode: TypeAlias = BinaryNode
+
+
+class Solution:
+    def isCompleteTree(self, root: TreeNode) -> bool:
+        return self.__method1(root)
+
+    def __method1(self, root: TreeNode) -> bool:
+        nodes: List[Tuple[TreeNode, int]] = [(root, 1)]
+        i = 0
+        while i < len(nodes):
+            node, index = nodes[i]
+            i += 1
+            if node:
+                nodes.append((node.left, 2 * index))
+                nodes.append((node.right, 2 * index + 1))
+        return nodes[-1][1] == len(nodes)
+
+
+if __name__ == "__main__":
+    tree = TreeNode.buildTree([None])
+
+    s = Solution()
+    print(s.isCompleteTree(tree))
